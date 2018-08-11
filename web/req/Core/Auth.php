553d68b09a9d219b2440c50554ad69d266505c61
dorakkosu <33192731+dorakkosu@users.noreply.github.com>
2018-08-11 21:14:02 +0900

    'test'

--- web/req/Core/Auth.php
+++ web/req/Core/Auth.php
@@ -37,7 +37,11 @@ class Auth
 			if (Configuration::$instance->storeSessionIntoDataStore)
 				SessionStore::useSessionStore();
 				
-			$redis = new Predis\Client(getenv('REDIS_URL'));
+			$redis = new Predis\Client(array(
+			    'host' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
+			    'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
+			    'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
+			));
 			$sessHandler = new RedisSessionHandler($redis);
 			session_set_save_handler($sessHandler);
 			
