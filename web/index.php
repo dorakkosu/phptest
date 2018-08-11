b1a6d40fff9bfa3956e1e2b3c43ed50d21a5424c
dorakkosu <33192731+dorakkosu@users.noreply.github.com>
2018-08-11 13:48:50 +0900

    'test'

--- web/index.php
+++ web/index.php
@@ -14,17 +14,5 @@ define("DEFAULT_HANDLER", "index");
 define("DEFAULT_ACTION", "index");
 define("SQL_DEBUG", false);
 
-require_once( "../vendor/autoload.php" );
-require_once( "../RedisSessionHandler.php" );
-
-$redis = new Predis\Client([
-    'host' => parse_url($_ENV['REDIS_URL'], PHP_URL_HOST),
-    'port' => parse_url($_ENV['REDIS_URL'], PHP_URL_PORT),
-    'password' => parse_url($_ENV['REDIS_URL'], PHP_URL_PASS),
-]);
-$sessHandler = new RedisSessionHandler($redis);
-session_set_save_handler($sessHandler);
-session_start();
-
 require APP_DIR . "App.php";
-?>	
+?>
